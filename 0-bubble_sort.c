dba1fe95ff78a2619d19ea8783c0d1bab8ed194d
doreenbett <doreenbett001@gmail.com>
2022-01-10 19:44:39 +0000

    Adding new files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+  *swap_func - swaps two elements
+  *@j: First integer to swap
+  *@k: Second integer to swap
+  */
+void swap_func(int *j, int *k)
+{
+	int temp;
+
+	temp = *j;
+	*j = *k;
+	*k = temp;
+}
+
+/**
+  *bubble_sort - sorts an array of integers in ascending order
+  *@array: pointer to the number of integers in an array
+  *@size: The length of the array
+  *
+  */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 1; i < len - 1; i++)
+	{
+		if (array[i] > array[i + 1])
+		{
+			swap_func(array + 1, array + i + 1);
+			print_array(array, size);
+		}
+	}
+	len--;
+}
