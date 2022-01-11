45fa631ef6107691845baf4c5f1ae8d0ebb7be74
doreenbett <doreenbett001@gmail.com>
2022-01-11 12:13:35 +0000

    Adding newfiles

--- sort.h
+++ sort.h
@@ -35,5 +35,8 @@ void print_list(const listint_t *list);
 
 /* sorting algorithms */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
